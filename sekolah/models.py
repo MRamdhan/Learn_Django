b233940708a005d909758683d70b98ed253b5925
admin <daniiidani883@gmail.com>
2023-01-18 13:41:45 +0700

    apa

--- sekolah/models.py
+++ sekolah/models.py
@@ -1,29 +1,35 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 class statussekolah(models.TextChoices):
-    SMA = 'SMA', _('Sekolah Menengah Atas')
-    SMK = 'SMK', _('Sekolah Menengah Kejuruan')
-    UNIVERSITAS = 'UNIV', _('Perguruan Tinggi')
+    SWASTA = 'Swasta', _('Swasta')
+    NEGERI = 'Negeri', _('Negeri')
 
 class sekolah(models.Model):
-    npsn =models.CharField(max_length=50)
+    npsn =models.CharField(max_length=50, default='')
     nama = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True,null=True)
     hp = models.CharField(max_length=254, blank=True,null=True)
     alamat = models.TextField(max_length=254, blank=True,null=True)
     provinsi = models.TextField(max_length=254, blank=True,null=True)
-    kota_kecamatan= models.TextField(max_length=254, blank=True,null=True)
+    kabupaten_kota= models.TextField(max_length=254, blank=True,null=True)
     status = models.CharField(
-        max_length=4,
+        max_length=10,
         choices=statussekolah.choices,
+        null=False,
+        default=''
     )
 
     #default
-    #create_by =
-    create_at =models.DateTimeField(auto_now_add=True)
-    update_at =models.DateTimeField(auto_now=True)
+    create_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name="sekolah_created_by")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nama
     
+    class Meta:
+        db_table = "sekolah"
+        ordering = ["-id"]
+        verbose_name_plural = "Sekolah"
--- sekolah/tests.py
+++ sekolah/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import sekolah
 
-# Create your tests here.
+class sekolahTestCase(TestCase):
+    def setUp(self):
+        sekolah.objects.create(npsn="2026822X", nama="SMKN 2 ", status="Negeri")
+    
+    def test_sekolah_cek_nama(self):
+        """ Cek nama sekolah """
+        smkn2 = sekolah.objects.get(nama="SMKN 2 ")
+        self.assertEqual(smkn2.nama, "SMKN 2 ")
