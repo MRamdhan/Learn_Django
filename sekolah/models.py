039e1e7d618dfea315f13936e0fb15712e12f497
admin <daniiidani883@gmail.com>
2023-01-18 11:18:02 +0700

    mengubah models sekolah

--- sekolah/models.py
+++ sekolah/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-class jenissekolah(models.TextChoices):
+class statussekolah(models.TextChoices):
     SMA = 'SMA', _('Sekolah Menengah Atas')
     SMK = 'SMK', _('Sekolah Menengah Kejuruan')
     UNIVERSITAS = 'UNIV', _('Perguruan Tinggi')
 
 class sekolah(models.Model):
+    npsn =models.CharField(max_length=50)
     nama = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True,null=True)
-    web = models.CharField(max_length=100)
     hp = models.CharField(max_length=254, blank=True,null=True)
-    alamat = models.TextField(default='Jawa Barat')
-    jenis_sekolah = models.CharField(
+    alamat = models.TextField(max_length=254, blank=True,null=True)
+    provinsi = models.TextField(max_length=254, blank=True,null=True)
+    kota_kecamatan= models.TextField(max_length=254, blank=True,null=True)
+    status = models.CharField(
         max_length=4,
-        choices=jenissekolah.choices,
+        choices=statussekolah.choices,
     )
 
     #default
